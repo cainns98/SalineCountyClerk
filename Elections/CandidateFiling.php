a9d9827487313d5f1cb2a84a08d9baac58d66cbe
cainns98 <cainns98@users.noreply.github.com>
2017-06-05 13:19:42 -0500

    Update CandidateFiling.php

--- Elections/CandidateFiling.php
+++ Elections/CandidateFiling.php
@@ -21,6 +21,7 @@
     echo __DIR__;
     echo getcwd() . "\n";
     chdir('public_html');
+    echo getcwd() . "\n";
     echo __DIR__;
     require("simple_html_dom.php");
     echo file_get_html('/partials/nav.html');
