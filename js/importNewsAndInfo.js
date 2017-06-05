56ef60902e8915a9680fcfcfe943528d13779d78
cainns98 <cainns98@users.noreply.github.com>
2017-06-05 08:55:54 -0500

    Update importNewsAndInfo.js

--- js/importNewsAndInfo.js
+++ js/importNewsAndInfo.js
@@ -1,6 +1,6 @@
 console.log('working');
 $.ajax({ url: '/partials/newsAndInfo.html', 
          success: function(data) { 
-             $("#newsAndInfo").html = data; 
+             $("#newsAndInfo").html(data); 
          } 
 });
