73e0b3efe75d1dcbac641f212d731aba89a9045f
cainns98 <cainns98@users.noreply.github.com>
2017-06-05 16:32:46 -0500

    Update CandidateFiling.php

--- Elections/CandidateFiling.php
+++ Elections/CandidateFiling.php
@@ -38,11 +38,11 @@
                     <div class="list-group">
                       <h4 class="list-group-item active">Candidate Reporting Documents</h4>
                       <a href="http://dpnfam.net/elecprep/wp-content/downloads/AR/Saline/18_Statement_of_Financial_Interest_2015.pdf" target="_blank" class="list-group-item">
-                          <span class="badge badge-btn"><button class="btn-link">Instructions</button></span>
+                          <span class="badge badge-btn"><button class="btn-link" onclick="location.href='http://google.com';">With Instructions</button></span>
                           Statement of Financial Interest
                       </a>
                       <a href="http://dpnfam.net/elecprep/wp-content/downloads/AR/Saline/04_CE_Cnty-Mun-SchlBd_Report_2015.pdf" target="_blank" class="list-group-item">
-                          <span class="badge badge-btn"><button class="btn-link">Instructions</button></span>
+                          <span class="badge badge-btn"><button class="btn-link" onclick="location.href='http://google.com';">With Instructions</button></span>
                           Campaign Contribution and Expenditure Report
                       </a>
                     </div>
