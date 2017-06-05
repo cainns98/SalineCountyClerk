93f34f883481b639d6c07f25339b56f5c7c3856b
cainns98 <cainns98@users.noreply.github.com>
2017-06-05 11:19:17 -0500

    Update and rename template.html to template.php

---
+++ template.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>PAGE TITLE</title>
+    <base href="http://salinecountyclerk.azurewebsites.net">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="css/main.css" />
+    
+    <script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>
+    <script type="text/javascript" src="js/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+</head>
+
+<body>
+    <?php 
+    require 'simple_html_dom.php';
+    echo file_get_html('partials/nav.html');
+    ?>
+    <div class="container">
+        <div class="row">
+            <!-- MAIN -->
+            <div class="col-md-9">
+                <div class="container-fluid">
+                
+                </div>
+            </div>
+            <!-- Side Bar -->
+            <div class="col-md-3">
+                <div class="container-fluid">
+                    <div class="panel panel-primary text-center">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Hours and Information</h3>
+                        </div>
+                        <div class="panel-body">
+                            8:00 AM - 4:30 PM <br /> Monday - Friday
+                            <hr /> Address:
+                            <br /> Saline County Clerk <br /> 215 North Main Street <br /> Benton, AR 72015 <br />
+                            <hr /> Phone: <a href="tel:+1-501-303-5630">501-303-5630</a>
+                        </div>
+                    </div>
+                    <div class="panel panel-primary text-center" id="newsAndInfo">
+                        <!-- Imported through JQuery -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer class="footer">
+        <div class="container text-center">
+            <a href="https://www.facebook.com/DougCurtisAR/"     style="margin-right: 25px;"><i class="fa fa-facebook"></i></a>
+            
+            <a href="https://twitter.com/DougCurtisAR"><i class="fa fa-twitter"></i></a>
+        </div>
+
+    </footer>
+    <script type="text/javascript" src="js/importNewsAndInfo.js"></script> 
+</body>
+
+</html>
