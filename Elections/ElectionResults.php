683e5d707754e6306d3a844a2bf154d3bb04e02f
cainns98 <cainns98@users.noreply.github.com>
2017-06-06 11:49:15 -0500

    Update ElectionResults.php

--- Elections/ElectionResults.php
+++ Elections/ElectionResults.php
@@ -46,7 +46,24 @@
                     </ul>
                     <div id="myTabContent" class="tab-content">
                       <div class="tab-pane fade active in" id="2017">
-                        <p>Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry richardson ex squid. Aliquip placeat salvia cillum iphone. Seitan aliquip quis cardigan american apparel, butcher voluptate nisi qui.</p>
+                        <div class="list-group">
+                          <h4 class="list-group-item active">Benton School Special Election - May 9, 2017</h4>
+                          <a href="http://dpnfam.net/elecprep/wp-content/downloads/AR/Saline/EV/Early%20Voting%20-%20Benton%20Special%20School%20Election.pdf" target="_blank" class="list-group-item">
+                              Location, Dates and Times
+                          </a>
+                          <a href="https://docs.google.com/spreadsheets/d/1LI1m4GZbxpOSYADHZPEaBj1NkldW36xU8hRGQrZz0VE/pub?gid=703524298&single=true&output=pdf" target="_blank" class="list-group-item">
+                              Early Voting Turnout
+                          </a>
+                        </div>
+                        <div class="list-group">
+                          <h4 class="list-group-item active">Bryant School Special Election - March 14, 2017</h4>
+                          <a href="http://dpnfam.net/elecprep/wp-content/downloads/AR/Saline/EV/Early%20Voting-Bryant%20School%20Special.pdf" target="_blank" class="list-group-item">
+                              Location, Dates and Times
+                          </a>
+                          <a href="https://docs.google.com/spreadsheets/d/1e8S1BmI9yZbWkn7Jk70oxDMcm9PXDyM3D3Y5eXNt8Fc/pub?gid=703524298&single=true&output=pdf" target="_blank" class="list-group-item">
+                              Early Voting Turnout
+                          </a>
+                        </div>
                       </div>
                       <div class="tab-pane fade" id="2016">
                         <p>Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum PBR. Homo nostrud organic, assumenda labore aesthetic magna delectus mollit.</p>
