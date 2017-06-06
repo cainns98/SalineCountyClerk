1d0bd79ac9bc913d683df3cff8bdb31bb54c6ec1
cainns98 <cainns98@users.noreply.github.com>
2017-06-06 15:07:34 -0500

    Update CandidateFiling.php

--- Elections/CandidateFiling.php
+++ Elections/CandidateFiling.php
@@ -13,7 +13,6 @@
     <script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>
     <script type="text/javascript" src="js/modernizr-2.8.3-respond-1.4.2.min.js"></script>
     <script type="text/javascript" src="js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="js/npm.js"></script>
     
     <?php
     chdir('../');
