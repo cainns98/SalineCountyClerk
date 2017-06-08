e8ebe0fcba519ceb2545e4a90c22430a59845fc1
cainns98 <cainns98@users.noreply.github.com>
2017-06-08 08:57:35 -0500

    Create ArkansasJudiciaryCourtConnect.php

---
+++ Probate/ArkansasJudiciaryCourtConnect.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>CourtConnect</title>
+    <base href="http://salinecountyclerk.azurewebsites.net">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="css/main.css" />
+    
+    <script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>
+    <script type="text/javascript" src="js/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+    <?php
+    chdir('../');
+    require 'simple_html_dom.php';
+    echo file_get_html('partials/favicons.html');
+    ?>
+</head>
+
+<body>
+    <?php
+    echo file_get_html('partials/nav.html');
+    ?>
+    <div class="container">
+        <div class="row">
+            <!-- MAIN -->
+            <div class="col-md-9">
+                <div class="container-fluid">
+                    <h1>Absentee Voting</h1>
+                    <br />
+                    <div class="list-group">
+                      <h4 class="list-group-item active">Links and Information</h4>
+                      <a href="https://caseinfo.aoc.arkansas.gov/cconnect/PROD/public/ck_public_qry_main.cp_main_idx" target="_blank" class="list-group-item">
+                          AOC CourtConnect Website
+                      </a>
+                      <a href="https://courts.arkansas.gov/administration/acap/courtconnect" target="_blank" class="list-group-item">
+                          Arkansas Judiciary CourtConnect – Overview
+                      </a>
+                      <a href="https://courts.arkansas.gov/administration/acap/courtconnect/help" target="_blank" class="list-group-item">
+                          Arkansas Judiciary CourtConnect – Help Overview
+                      </a>
+                      <a href="https://caseinfo.aoc.arkansas.gov/docs/helppublic.htm" target="_blank" class="list-group-item">
+                          Arkansas Judiciary CourtConnect – Help
+                      </a>
+                    </div>
+                </div>
+            </div>
+            <!-- Side Bar -->
+            <div class="col-md-3">
+                <div class="container-fluid">
+                    <div class="panel panel-primary text-center">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Hours and Information</h3>
+                        </div>
+                        <div class="panel-body">
+                            8:00 AM - 4:30 PM <br /> Monday - Friday
+                            <hr /> Address:
+                            <br /> Saline County Clerk <br /> 215 North Main Street <br /> Benton, AR 72015 <br />
+                            <hr /> Phone: <a href="tel:+1-501-303-5630">501-303-5630</a>
+                        </div>
+                    </div>
+                    <div class="panel panel-primary text-center">
+                        <?php
+                        echo file_get_html('partials/newsAndInfo.html');
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer class="footer">
+        <div class="container text-center">
+            <a href="https://www.facebook.com/DougCurtisAR/"     style="margin-right: 25px;"><i class="fa fa-facebook"></i></a>
+            
+            <a href="https://twitter.com/DougCurtisAR"><i class="fa fa-twitter"></i></a>
+        </div>
+
+    </footer>
+</body>
+
+</html>
