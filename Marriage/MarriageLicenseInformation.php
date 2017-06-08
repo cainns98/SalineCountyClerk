42519b8bd4dc43e19c3b2fcd4f7b77e8aff66131
cainns98 <cainns98@users.noreply.github.com>
2017-06-08 08:28:49 -0500

    Create MarriageLicenseInformation.php

--- Marriage/MarriageLicenseInformation.php
+++ Marriage/MarriageLicenseInformation.php
@@ -43,7 +43,7 @@
                                 <hr> Address:
                                 <br> Saline County Clerk's Office <br> 215 North Main Street, Suite 9 <br> Benton, AR 72015 <br>
                                 <hr> Phone: <a href="tel:+1-501-303-5630">501-303-5630</a>
-                                <hr> Both parties must be present with government issued photo I.D.
+                                <hr> Cost is 60.00 (cash, local check, or credit/debit plus $3 processing fee)
                             </div>
                           </div>
                       </div>
@@ -54,7 +54,8 @@
                                         <h3 class="panel-title">Remember</h3>
                                     </div>
                                     <div class="panel-body">
-                                        Marriage license is valid sixty (60) days from date of issuance. The license must be returned to our office for recording, whether used, unused within sixty (60) days.
+                                        Both parties must be present with government issued photo I.D.
+                                        <hr> Marriage license is valid sixty (60) days from date of issuance. The license must be returned to our office for recording, whether used, unused within sixty (60) days.
                                     </div>
                                 </div>
                                 <div class="panel panel-warning text-center">
