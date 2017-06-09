b8b027dcc9f58f2e543d861799365cc1d9d6fd81
cainns98 <cainns98@users.noreply.github.com>
2017-06-09 08:22:26 -0500

    Create ElectionsCalendar.php

---
+++ VoterInformation/ElectionsCalendar.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Elections Calendar</title>
+    <base href="http://salinecountyclerk.azurewebsites.net">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="css/main.css" />
+    
+    <script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>
+    <script type="text/javascript" src="js/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+    <?php
+    chdir('../');
+    require 'simple_html_dom.php';
+    echo file_get_html('partials/favicons.html');
+    ?>
+</head>
+
+<body>
+    <?php
+    echo file_get_html('partials/nav.html');
+    ?>
+    <div class="container">
+        <div class="row">
+            <!-- MAIN -->
+            <div class="col-md-9">
+                <div class="container-fluid">
+                    <h1>Saline County Elections Calendar</h1>
+                    <br />
+                    <iframe src="https://calendar.google.com/calendar/embed?src=v7fqv20af9oq01eh1s8q2bo010%40group.calendar.google.com&amp;ctz=America/Chicago" style="border: 0" width="600" height="450" frameborder="0" scrolling="no"></iframe>
+                </div>
+            </div>
+            <!-- Side Bar -->
+            <div class="col-md-3">
+                <div class="container-fluid">
+                    <div class="panel panel-primary text-center">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Hours and Information</h3>
+                        </div>
+                        <div class="panel-body">
+                            8:00 AM - 4:30 PM <br /> Monday - Friday
+                            <hr /> Address:
+                            <br /> Saline County Clerk <br /> 215 North Main Street <br /> Benton, AR 72015 <br />
+                            <hr /> Phone: <a href="tel:+1-501-303-5630">501-303-5630</a>
+                        </div>
+                    </div>
+                    <div class="panel panel-primary text-center">
+                        <?php
+                        echo file_get_html('partials/newsAndInfo.html');
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer class="footer">
+        <div class="container text-center">
+            <a href="https://www.facebook.com/DougCurtisAR/"     style="margin-right: 25px;"><i class="fa fa-facebook"></i></a>
+            
+            <a href="https://twitter.com/DougCurtisAR"><i class="fa fa-twitter"></i></a>
+        </div>
+
+    </footer>
+</body>
+
+</html>
