bf9fe94899055515df0072a1607b7be6ff0db59c
cainns98 <cainns98@users.noreply.github.com>
2017-06-09 09:16:30 -0500

    Create AreYouRegistered.php

---
+++ VoterInformation/AreYouRegistered.php
@@ -0,0 +1,272 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Are You Registered?</title>
+    <base href="http://salinecountyclerk.azurewebsites.net">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="css/main.css" />
+    
+    <script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>
+    <script type="text/javascript" src="js/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+    <?php
+    chdir('../');
+    require 'simple_html_dom.php';
+    echo file_get_html('partials/favicons.html');
+    ?>
+</head>
+
+<body>
+    <?php
+    echo file_get_html('partials/nav.html');
+    ?>
+    <div class="container">
+        <div class="row">
+            <!-- MAIN -->
+            <div class="col-md-9">
+                <div class="container-fluid">
+                    <h1>Are You Registered?</h1>
+                    <br />
+                    <form name="registrantSearchForm" method="post" action="https://www.voterview.ar-nova.org/VoterView/RegistrantSearch.do" onsubmit="this.action.value='Search';return validateRegistrantSearchForm(this)">
+
+                      <input type="hidden" name="action" value="">
+                      <span class="inner-title">Registrant Search</span>
+                      <div class="field-row field-input">
+                      </div>
+
+                      <div class="field-row">
+                        <div class="field-input">
+                          <label for="nameFirst">
+                            <span class="required ">*</span>
+                            <span class="">First Name</span>
+                          </label>
+                          <br>
+                          <input type="text" name="nameFirst" maxlength="25" size="22" value="test" id="nameFirst" class="input-box">
+                        </div>
+
+                        <div class="field-input">
+                            <label for="nameLast">
+                              <span class="required ">*</span>
+                              <span class="">Last Name</span>
+                            </label>
+                            <br>
+                            <input type="text" name="nameLast" maxlength="25" size="22" value="ing" id="nameLast" class="input-box">
+                        </div>
+                      </div>
+
+                      <div class="field-row field-input">
+                          <label for="dobMonth">
+                            <span class="required ">*</span>
+                            <span class="">Date of Birth</span>
+                          </label>
+                          <br>
+                          <select name="dobMonth" size="1" id="dobMonth" class="input-box" title="Select your birth month">
+                              <option value="0" selected="selected">Month</option>
+                              <option value="01">Jan</option>
+                              <option value="02">Feb</option>
+                              <option value="03">Mar</option>
+                              <option value="04">Apr</option>
+                              <option value="05">May</option>
+                              <option value="06">Jun</option>
+                              <option value="07">Jul</option>
+                              <option value="08">Aug</option>
+                              <option value="09">Sep</option>
+                              <option value="10">Oct</option>
+                              <option value="11">Nov</option>
+                              <option value="12">Dec</option>
+                           </select>
+                           <select name="dobDay" size="1" id="dobDay" class="input-box" title="Select your birth day">
+                              <option value="0" selected="selected">Day</option>
+                              <option value="01">01</option>
+                              <option value="02">02</option>
+                              <option value="03">03</option>
+                              <option value="04">04</option>
+                              <option value="05">05</option>
+                              <option value="06">06</option>
+                              <option value="07">07</option>
+                              <option value="08">08</option>
+                              <option value="09">09</option>
+                              <option value="10">10</option>
+                              <option value="11">11</option>
+                              <option value="12">12</option>
+                              <option value="13">13</option>
+                              <option value="14">14</option>
+                              <option value="15">15</option>
+                              <option value="16">16</option>
+                              <option value="17">17</option>
+                              <option value="18">18</option>
+                              <option value="19">19</option>
+                              <option value="20">20</option>
+                              <option value="21">21</option>
+                              <option value="22">22</option>
+                              <option value="23">23</option>
+                              <option value="24">24</option>
+                              <option value="25">25</option>
+                              <option value="26">26</option>
+                              <option value="27">27</option>
+                              <option value="28">28</option>
+                              <option value="29">29</option>
+                              <option value="30">30</option>
+                              <option value="31">31</option>
+                            </select>
+                            <select name="dobYear" size="1" id="dobYear" class="input-box" title="Select your birth year">
+                              <option value="0" selected="selected">Year</option>
+                              <option value="2001">2001</option>
+                              <option value="2000">2000</option>
+                              <option value="1999">1999</option>
+                              <option value="1998">1998</option>
+                              <option value="1997">1997</option>
+                              <option value="1996">1996</option>
+                              <option value="1995">1995</option>
+                              <option value="1994">1994</option>
+                              <option value="1993">1993</option>
+                              <option value="1992">1992</option>
+                              <option value="1991">1991</option>
+                              <option value="1990">1990</option>
+                              <option value="1989">1989</option>
+                              <option value="1988">1988</option>
+                              <option value="1987">1987</option>
+                              <option value="1986">1986</option>
+                              <option value="1985">1985</option>
+                              <option value="1984">1984</option>
+                              <option value="1983">1983</option>
+                              <option value="1982">1982</option>
+                              <option value="1981">1981</option>
+                              <option value="1980">1980</option>
+                              <option value="1979">1979</option>
+                              <option value="1978">1978</option>
+                              <option value="1977">1977</option>
+                              <option value="1976">1976</option>
+                              <option value="1975">1975</option>
+                              <option value="1974">1974</option>
+                              <option value="1973">1973</option>
+                              <option value="1972">1972</option>
+                              <option value="1971">1971</option>
+                              <option value="1970">1970</option>
+                              <option value="1969">1969</option>
+                              <option value="1968">1968</option>
+                              <option value="1967">1967</option>
+                              <option value="1966">1966</option>
+                              <option value="1965">1965</option>
+                              <option value="1964">1964</option>
+                              <option value="1963">1963</option>
+                              <option value="1962">1962</option>
+                              <option value="1961">1961</option>
+                              <option value="1960">1960</option>
+                              <option value="1959">1959</option>
+                              <option value="1958">1958</option>
+                              <option value="1957">1957</option>
+                              <option value="1956">1956</option>
+                              <option value="1955">1955</option>
+                              <option value="1954">1954</option>
+                              <option value="1953">1953</option>
+                              <option value="1952">1952</option>
+                              <option value="1951">1951</option>
+                              <option value="1950">1950</option>
+                              <option value="1949">1949</option>
+                              <option value="1948">1948</option>
+                              <option value="1947">1947</option>
+                              <option value="1946">1946</option>
+                              <option value="1945">1945</option>
+                              <option value="1944">1944</option>
+                              <option value="1943">1943</option>
+                              <option value="1942">1942</option>
+                              <option value="1941">1941</option>
+                              <option value="1940">1940</option>
+                              <option value="1939">1939</option>
+                              <option value="1938">1938</option>
+                              <option value="1937">1937</option>
+                              <option value="1936">1936</option>
+                              <option value="1935">1935</option>
+                              <option value="1934">1934</option>
+                              <option value="1933">1933</option>
+                              <option value="1932">1932</option>
+                              <option value="1931">1931</option>
+                              <option value="1930">1930</option>
+                              <option value="1929">1929</option>
+                              <option value="1928">1928</option>
+                              <option value="1927">1927</option>
+                              <option value="1926">1926</option>
+                              <option value="1925">1925</option>
+                              <option value="1924">1924</option>
+                              <option value="1923">1923</option>
+                              <option value="1922">1922</option>
+                              <option value="1921">1921</option>
+                              <option value="1920">1920</option>
+                              <option value="1919">1919</option>
+                              <option value="1918">1918</option>
+                              <option value="1917">1917</option>
+                              <option value="1916">1916</option>
+                              <option value="1915">1915</option>
+                              <option value="1914">1914</option>
+                              <option value="1913">1913</option>
+                              <option value="1912">1912</option>
+                              <option value="1911">1911</option>
+                              <option value="1910">1910</option>
+                              <option value="1909">1909</option>
+                              <option value="1908">1908</option>
+                              <option value="1907">1907</option>
+                              <option value="1906">1906</option>
+                              <option value="1905">1905</option>
+                              <option value="1904">1904</option>
+                              <option value="1903">1903</option>
+                              <option value="1902">1902</option>
+                              <option value="1901">1901</option>
+                              <option value="1900">1900</option>
+                              <option value="1899">1899</option>
+                              <option value="1898">1898</option>
+                            </select>
+                      </div>
+                      <div class="field-row">
+                      </div>
+
+                      <div class="search-explanation">
+                        <span class="required">*</span>
+                        Denotes a required field.
+                      </div>
+                      <div class="search-button-box">
+                        <input type="submit" name="search" value="Search" class="search-button">	
+                      </div>
+
+                    </form>
+                </div>
+            </div>
+            <!-- Side Bar -->
+            <div class="col-md-3">
+                <div class="container-fluid">
+                    <div class="panel panel-primary text-center">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Hours and Information</h3>
+                        </div>
+                        <div class="panel-body">
+                            8:00 AM - 4:30 PM <br /> Monday - Friday
+                            <hr /> Address:
+                            <br /> Saline County Clerk <br /> 215 North Main Street <br /> Benton, AR 72015 <br />
+                            <hr /> Phone: <a href="tel:+1-501-303-5630">501-303-5630</a>
+                        </div>
+                    </div>
+                    <div class="panel panel-primary text-center">
+                        <?php
+                        echo file_get_html('partials/newsAndInfo.html');
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer class="footer">
+        <div class="container text-center">
+            <a href="https://www.facebook.com/DougCurtisAR/"     style="margin-right: 25px;"><i class="fa fa-facebook"></i></a>
+            
+            <a href="https://twitter.com/DougCurtisAR"><i class="fa fa-twitter"></i></a>
+        </div>
+
+    </footer>
+</body>
+
+</html>
