e6d5a3c5f32aea387f736806718e1ac56dd4c6a5
cainns98 <cainns98@users.noreply.github.com>
2017-06-09 08:37:11 -0500

    Create iframeFullWidth.js

--- js/iframeFullWidth.js
+++ js/iframeFullWidth.js
@@ -1,28 +1,13 @@
 $( document ).ready(function() {
     (function($, viewport){
         var width = $(".width-match-element").first().width();
+        $(".full-width").attr("width", width);
         
-        // For XS
-        if( viewport.is('<sm') ) {
-            $(".split-list-group-item .left").toggleClass( "vertical", true );
-        }
-
-        // For SM, MD, and LG
-        if( viewport.is('>=sm') ) {
-            $(".split-list-group-item .left").toggleClass( "horizontal", true );
-        }
-
         // Execute code each time window size changes
         $(window).resize(
             viewport.changed(function() {
-                if( viewport.is('<sm') ) {
-                    $(".split-list-group-item .left").toggleClass( "vertical", true );
-                    $(".split-list-group-item .left").toggleClass( "horizontal", false );
-                }
-                if( viewport.is('>=sm') ) {
-                    $(".split-list-group-item .left").toggleClass( "horizontal", true );
-                    $(".split-list-group-item .left").toggleClass( "vertical", false );
-                }
+                var width = $(".width-match-element").first().width();
+                $(".full-width").attr("width", width);
             })
         ); 
 
