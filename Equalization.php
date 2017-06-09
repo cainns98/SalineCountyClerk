498fb2e9bc8b4d894779c1aae62e368bd2d460cc
cainns98 <cainns98@users.noreply.github.com>
2017-06-09 13:49:22 -0500

    Create Equalization.php

--- Equalization.php
+++ Equalization.php
@@ -30,11 +30,11 @@
             <!-- MAIN -->
             <div class="col-md-9">
                 <div class="container-fluid">
-                    <h1>2017 Equalization Timeline</h1>
+                    <h1 class="width-match-element">2017 Equalization Timeline</h1>
                     <br />
                     <ul class="timeline">
                       <li>
-                        <div class="timeline-badge"><i class="glyphicon glyphicon-envelope"></i></div>
+                        <div class="timeline-badge primary"><i class="glyphicon glyphicon-envelope"></i></div>
                         <div class="timeline-panel">
                           <div class="timeline-heading">
                             <h4 class="timeline-title">July</h4>
@@ -44,20 +44,21 @@
                           </div>
                         </div>
                       </li>
-                      <li class="timeline-inverted">
-                        <div class="timeline-badge warning"><i class="glyphicon glyphicon-credit-card"></i></div>
+                      <li class="timeline-inverted timeline-right">
+                        <div class="timeline-badge success"><i class="glyphicon glyphicon-calendar"></i></div>
                         <div class="timeline-panel">
                           <div class="timeline-heading">
-                            <h4 class="timeline-title">Mussum ipsum cacilds</h4>
+                            <h4 class="timeline-title">July 10th</h4>
                           </div>
                           <div class="timeline-body">
-                            <p>Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra lá , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. Mé faiz elementum girarzis, nisi eros vermeio, in elementis mé pra quem é amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.</p>
-                            <p>Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. Interagi no mé, cursus quis, vehicula ac nisi. Aenean vel dui dui. Nullam leo erat, aliquet quis tempus a, posuere ut mi. Ut scelerisque neque et turpis posuere pulvinar pellentesque nibh ullamcorper. Pharetra in mattis molestie, volutpat elementum justo. Aenean ut ante turpis. Pellentesque laoreet mé vel lectus scelerisque interdum cursus velit auctor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac mauris lectus, non scelerisque augue. Aenean justo massa.</p>
+                            <p>County Clerk's Office begins making appointments to go before the EQ Board</p>
+                            <hr>
+                            <p>Call <a href="tel:+1-501-303-5630" class="btn btn-primary">(501)303-5630</a> to schedule an appointment.</p>
                           </div>
                         </div>
                       </li>
                       <li>
-                        <div class="timeline-badge danger"><i class="glyphicon glyphicon-credit-card"></i></div>
+                        <div class="timeline-badge primary"><i class="glyphicon glyphicon-credit-card"></i></div>
                         <div class="timeline-panel">
                           <div class="timeline-heading">
                             <h4 class="timeline-title">Mussum ipsum cacilds</h4>
