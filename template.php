f268293541bc56a1024b52b2296391ee286b8fbf
cainns98 <cainns98@users.noreply.github.com>
2019-03-19 10:31:48 -0500

    change to salinecountyclerk.azurewebsites.net

--- template.php
+++ template.php
@@ -3,7 +3,7 @@
 
 <head>
     <title>PAGE TITLE</title>
-    <base href="http://www.salineclerk.com">
+    <base href="http://salinecountyclerk.azurewebsites.net">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
